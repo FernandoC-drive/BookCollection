e455f2e5e366a34e2ccb314805fecbae63585b81
FernandoC-drive <fernando.c@tamu.edu>
2026-02-02 10:25:46 -0600

    Final adjustments for book collection 4

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
-  has_many :users, through: :user_books
-  has_many :user_books
-
-  validates :title, :author, :price, :published_date, presence: true
 end
