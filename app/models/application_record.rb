b40bf91e87307006a2dd63bef86e018bdab70db7
FernandoC-drive <fernando.c@tamu.edu>
2026-02-02 09:47:43 -0600

    New book collection 4 changes

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  has_many :users, through: :user_books
+  has_many :user_books
+
+  validates :title, :author, :price, :published_date, presence: true
 end
